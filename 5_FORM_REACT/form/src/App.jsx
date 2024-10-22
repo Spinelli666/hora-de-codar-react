82e6df5cf58350011ce4193e3c9347b1da301c88
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-10-22 15:05:38 -0300

    (feat): Att do curso

---
+++ 5_FORM_REACT/form/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyForm from './components/MyForm'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Form em React</h1>
+      <MyForm userName="Bernardo" userEmail="bernardo666@gmail.com" />
+    </div>
+  )
+}
+
+export default App
