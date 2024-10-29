0d6c9bd4b2937ed0c70027b62dfa5bc6b6f0df2d
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-10-29 11:26:48 -0300

    (feact): Att do Curso

---
+++ 7_REACT_ROUTER/router/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+import { Outlet } from 'react-router-dom'
+
+// 5 - Link entre paginas
+import Navbar from './components/Navbar.jsx'
+
+// 9 - search params
+import SearchForm from './components/SearchForm.jsx'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <SearchForm />
+      <Outlet />
+      <p>Footer</p>
+    </div>
+  )
+}
+
+export default App
